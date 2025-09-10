a4d85c01bf3756d18fafd41bbe13645f1bc2f4a4
Ravindu62 <pasan62nanayakkara@gmail.com>
2025-09-11 01:12:26 +0530

    feat: setup basic express server and root endpoint

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
